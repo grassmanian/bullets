a00d40cbcaf0d6878a178c1e52887eb215f54370
grassmanian <52893991+grassmanian@users.noreply.github.com>
2020-01-04 20:38:47 -0500

    Add logic for picking date automatically

--- scraper.py
+++ scraper.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 import requests
+from datetime import datetime, timedelta
 
-urls = ("https://www.daytonlatinmass.org/wp-content/uploads/2019/12/2019-12-29.pdf", 
-        "http://www.emmanuelcatholic.com/wp-content/uploads/2019/12/Bulletin-122919.pdf",
-        "https://daytonxii.org/wp-content/uploads/2020/01/January-5-2020-Bulletin.pdf",
-        "https://www.byzantinecolumbus.com/bulletins/b20191229.pdf")
+# if today is not sunday, go to previous week, else use this week, 
+# if this week fails, downlaod last weeks
+date = datetime.now()
+date -= timedelta((date.weekday() + 1) % 7)
+    
+y = str(date.year)
+m = str(date.month)
+d = str(date.day)
+
+urls = ("https://www.daytonlatinmass.org/wp-content/uploads/{y}/{m}/{y}-{m}-{d}.pdf".format(y = y, m = m.zfill(2), d = d.zfill(2)), 
+        "http://www.emmanuelcatholic.com/wp-content/uploads/{year}/{m}/Bulletin-{m}{d}{y}.pdf".format(year = y, y = y[-2:], m = m.zfill(2), d = d.zfill(2)), 
+        "https://daytonxii.org/wp-content/uploads/{y}/{m}/{month}-{d}-{y}-Bulletin.pdf".format(y = y, m = m, d = d, month = date.strftime("%B")),
+        "https://www.byzantinecolumbus.com/bulletins/b{y}{m}{d}.pdf".format(y=y, m=m.zfill(2), d=d.zfill(2)) )
 
 for url in urls:
     response = requests.get(url, headers={ "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36" })
-    print(response.status_code)
+    # if this prints something other than 200, be wary
+    if response.status_code == 200:
+        print(url, response.status_code)
     filename = response.url
     last = filename.rfind('/')
 
