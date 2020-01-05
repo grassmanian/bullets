61ce2593ee94bac8d717a8add1bf8bdd9a2323ba
grassmanian <52893991+grassmanian@users.noreply.github.com>
2020-01-04 19:20:36 -0500

    Fix typo in tuple of urls

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import requests
 
 urls = ("https://www.daytonlatinmass.org/wp-content/uploads/2019/12/2019-12-29.pdf", 
         "http://www.emmanuelcatholic.com/wp-content/uploads/2019/12/Bulletin-122919.pdf",
-        "https://daytonxii.org/wp-content/uploads/2020/01/January-5-2020-Bulletin.pdf"
+        "https://daytonxii.org/wp-content/uploads/2020/01/January-5-2020-Bulletin.pdf",
         "https://www.byzantinecolumbus.com/bulletins/b20191229.pdf")
 
 for url in urls:
